6c569d8e1550132eafd8925e7be683c82f94be61
mike <mike@slimm.com>
2025-02-24 18:06:01 -0800

    chore: log cleanup

--- components/giphy-results-grid.tsx
+++ components/giphy-results-grid.tsx
@@ -5,7 +5,6 @@ interface Props {
 }
 
 export default function GifphyResultsGrid ({results}: Props) {
-    console.log(results);
     return (<div className="flex flex-wrap">
         {results.map((result) => {
             return (<div key={result.id} className="mx-4 my-4 bg-gray-200 minh-12">
